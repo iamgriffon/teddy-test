bd3abae1511dd9596dff32783b59959a83df9a4c
Gustavo Dupin <guusilveira@gmail.com>
2025-02-17 20:34:36 -0300

    [ REFAC ]#19- Instala tailwind e ajusta outras dependências do projeto

---
+++ front/src/components/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold font-inter">Hello World</h1>
+    </main>
+  )
+}
+
+export default App
