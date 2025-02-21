3c8bc55b9bf2fa773d42129c830570a376c48217
Gustavo Dupin <gustavo.dupin@orulo.com.br>
2025-02-20 23:30:56 -0300

    [ HOTFIX ] #63 - Ajusta instalação novamente

--- init-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-for db in teddy teddy_test; do
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE $db;
-    GRANT ALL PRIVILEGES ON DATABASE $db TO "$POSTGRES_USER";
-EOSQL
-done
